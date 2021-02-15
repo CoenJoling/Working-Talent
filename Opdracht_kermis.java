9d9c31b8d1b15c10595edd6b185b0d0f7304aa34
CoenJoling <78420826+CoenJoling@users.noreply.github.com>
2021-02-15 08:58:32 +0100

    Add files via upload

---
+++ Opdracht_kermis.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Opdracht_kermis {
+	public static void main(String[] args) {
+		
+		
+		Attractie kermis = new Attractie();
+		kermis.draaien();
+		
+		
+	}
+
+}
+
+class Attractie{
+	
+	void draaien() {
+		boolean draaienMaster=true;
+		while (draaienMaster=true) {
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Welkom op de kermis! In welke attractie wil je gaan? (1-6)");
+			String keuze = sc.next();
+			int intkeuze=Integer.parseInt(keuze)-1;		
+			String attractie [] = {"botsauto","spin","spiegelpaleis","spookhuis","hawaii","ladderklimmen"};
+			System.out.println("Veel plezier in: "+attractie[intkeuze]);
+		if (draaienMaster=false) {
+			break;
+		}
+		}
+			
+		}
+}
+
+class botsauto extends Attractie{
+	
+}
+
+class spin extends Attractie{
+	
+}
+
+class spiegelpaleis extends Attractie{
+	
+}
+
+class spookhuis extends Attractie{
+	
+}
+
+class hawaii extends Attractie{
+	
+}
+
+class ladderklimmen extends Attractie{
+	
+}
